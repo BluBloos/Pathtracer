65bccdb5224d831f7e90b3eaf009cdcebfd5a9b7
BluBloos <cnoah1705@gmail.com>
2024-07-16 12:53:22 -0400

    Remove optimization in the build.

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-cl /O2 /Z7 /I include /I include/external /std:c++20 /FePathtracer src/win32_main.cpp src/inf_forge_win.c /link User32.lib DXGI.lib Gdi32.lib
+cl /Z7 /I include /I include/external /std:c++20 /FePathtracer src/win32_main.cpp src/inf_forge_win.c /link User32.lib DXGI.lib Gdi32.lib
